7f6fddcc4a7b04553c7a66dbb4b4ad95b36b0326
Dídac <dimolsan13@gmail.com>
2026-08-11 16:55:41 +0200

    Delete Funcionament Cpp i Compilar directory

--- Funcionament Cpp i Compilar/Code/HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-	cout << "Hello World!" << endl;
-	// cout: Console OUTput
-	// << operador logic del cout (significa mostra a la consola el que ve després)
-	// endl: paraula reservada de la llibreria std, es un caracter que significa fi de linia (end line)
-}
